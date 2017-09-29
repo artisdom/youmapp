6689dc9444ef480a3d8322fd9bdede9929ee5025
leeggco <leeggco@users.noreply.github.com>
2017-09-29 15:44:55 +0800

    build

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,5 +3,18 @@
 const app = getApp()
 
 Page({
-    
+    data: {
+        imgUrls: [
+            'https://topic.camel.com.cn/module/wap_index_focus_201709/7645c28f-9674-49f0-8490-dca3f6237496.jpg?d=1',
+            'https://topic.camel.com.cn/module/wap_index_focus_201709/cb445730-dda3-466e-ab17-60f1f6ca1cb7.jpg',
+            'https://topic.camel.com.cn/module/wap_index_focus_201709/92b890bb-7182-4ba4-8bc7-3ffd7ebbcf1c.jpg'
+        ],
+        indicatorDots: true,  
+        indicatorColor: '#fff',
+        indicatorActiveColor: '#d2ab44',
+        autoplay: true,
+        interval: 5000,
+        duration: 500,
+        circular: true
+    }
 })
---
+++ pages/product/productDetail/productDetail.js
@@ -0,0 +1,17 @@
+// pages/product/productDetail/productDetail.js
+Page({
+    data: {
+        imgUrls: [
+            'https://img01.camel.com.cn/product/image/A712357085/6fb33911-3ca6-4487-87b0-ce954573b6dd.jpg',
+            'https://img01.camel.com.cn/product/image/A712357085/c8568bbd-5fcf-4c15-b143-f8e022ddbc18.jpg',
+            'https://img01.camel.com.cn/product/image/A712357085/e9e1b0dc-c431-486a-a410-5dcd1c6b7c21.jpg'
+        ],
+        indicatorDots: true,
+        indicatorColor: '#fff',
+        indicatorActiveColor: '#d2ab44',
+        autoplay: true,
+        interval: 5000,
+        duration: 500,
+        circular: true
+    }
+})
